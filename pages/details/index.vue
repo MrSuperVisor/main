b29375d41209c7cdd5d4d285951df43580910701
MrSuperVisor <supvisns@gmail.com>
2022-03-21 17:27:18 +0500

    some additional work

---
+++ pages/details/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div dark class="space">
+    <h1>
+      Page not found
+    </h1>
+    <NuxtLink to="/">
+      Retun to main
+    </NuxtLink>
+  </div>
+</template>
+
+<scriptlang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    mounted: () => {
+        setTimeout(() => {
+            this.$router.replace({path: '/'})
+        }, 3000);
+        
+    }
+})
+</script>
+
+<style scoped>
+.space {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  font-size: 20px;
+}
+</style>
